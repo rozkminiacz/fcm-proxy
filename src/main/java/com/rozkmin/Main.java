f9d6bcbbb4135325db51e6c3b40996ebacc7abfc
rozkminiacz <jaroslaw.michalik.rozkmin@gmail.com>
2017-06-05 18:49:00 +0200

    Updated ports

--- src/main/java/com/rozkmin/Main.java
+++ src/main/java/com/rozkmin/Main.java
@@ -1,11 +1,12 @@
 package com.rozkmin;
 
 import com.rozkmin.controllers.ControllerModule;
+import spark.Spark;
 
 public class Main {
 
     public static void main(String[] args) {
-//        Spark.port(81);
+        Spark.port(8080);
         App app = new App(ControllerModule.getInstance().getControllers());
         app.start();
     }
