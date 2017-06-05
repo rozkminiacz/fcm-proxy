ca81dda7a6ea2e80e258da0335abdd0c48c5e1e1
Jarosław Michalik <jmichalik@sointeractive.pl>
2017-06-05 17:58:16 +0200

    Changed port back to 80

--- src/main/java/com/rozkmin/Main.java
+++ src/main/java/com/rozkmin/Main.java
@@ -1,12 +1,11 @@
 package com.rozkmin;
 
 import com.rozkmin.controllers.ControllerModule;
-import spark.Spark;
 
 public class Main {
 
     public static void main(String[] args) {
-        Spark.port(80);
+//        Spark.port(81);
         App app = new App(ControllerModule.getInstance().getControllers());
         app.start();
     }
