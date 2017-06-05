337a490c95ea5f7c25762f2225cbdee42987e442
rozkminiacz <rozkminiacz@users.noreply.github.com>
2017-06-05 18:46:12 +0200

    Update Main.java

--- src/main/java/com/rozkmin/Main.java
+++ src/main/java/com/rozkmin/Main.java
@@ -1,12 +1,12 @@
-package com.rozkmin;
-
-import com.rozkmin.controllers.ControllerModule;
-
-public class Main {
-
-    public static void main(String[] args) {
-//        Spark.port(81);
-        App app = new App(ControllerModule.getInstance().getControllers());
-        app.start();
-    }
-}
+ package com.rozkmin;
+ 
+ import com.rozkmin.controllers.ControllerModule;
++import spark.Spark;
+ 
+ public class Main {
+ 
+     public static void main(String[] args) {
+         Spark.port(8080);
+         App app = new App(ControllerModule.getInstance().getControllers());
+         app.start();
+     }
